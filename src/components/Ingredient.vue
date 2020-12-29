7fba3e01c9bf524ca431fe7ed79b73fd5f6788b4
samsambutdifferent <sam@solutions.to>
2020-12-29 01:40:02 +0000

    comment out match

--- src/components/Ingredient.vue
+++ src/components/Ingredient.vue
@@ -6,7 +6,7 @@
         <input class="col-span-2 bg-blue-200 p-2 rounded"
           type="number" name="weight" v-model="weightValue" placeholder="weight (g)"
           autocomplete="off">
-        <div class="col-span-2 bg-blue-200 p-2 rounded">matched: {{matched}}</div>
+        <!-- <div class="col-span-2 bg-blue-200 p-2 rounded">{{matched}}</div> -->
         <div class="col-span-1 p-2">
             <button class="px-3 py-1 bg-red-200 rounded" @click="remove">-</button>
         </div>
