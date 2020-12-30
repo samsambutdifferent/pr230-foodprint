9fa5e4fcdbf55c41aba3fb990f172a36e79bc875
samsambutdifferent <sam@solutions.to>
2020-12-30 00:40:34 +0000

    add axios to ingredient

--- src/components/Ingredient.vue
+++ src/components/Ingredient.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="grid grid-cols-7 gap-4">
         <input class="col-span-4 md:col-span-2 bg-blue-200 p-2 rounded"
-          type="text" name="name" v-model="nameValue" placeholder="name"
+          type="text" name="name" v-model="nameValue" @change="() => matchSemantic(nameValue)" placeholder="name"
           autocomplete="off">
         <input class="col-span-2 bg-blue-200 p-2 rounded"
           type="number" name="weight" v-model="weightValue" placeholder="weight (g)"
           autocomplete="off">
-        <!-- <div class="col-span-2 bg-blue-200 p-2 rounded">{{matched}}</div> -->
+        <div class="col-span-2 bg-blue-200 p-2 rounded">{{matchedValue}}</div>
         <div class="col-span-1 p-2">
             <button class="px-3 py-1 bg-red-200 rounded" @click="remove">-</button>
         </div>
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'Ingredient',
@@ -22,9 +23,17 @@ export default {
     return {
         nameValue: this.name,
         weightValue: this.weight,
+        matchedValue: this.matched
     }
-  },  
+  },
   methods: {
+      async matchSemantic(name) {
+          let mx = await axios.post(process.env.VUE_APP_SEMANTIC_MATCHER_MATCH, {name: name})
+            .then(function (response) {
+              return response.data;
+            });
+            this.matchedValue = mx.matched
+      },
       remove() {
           this.$emit('remove')
       }
