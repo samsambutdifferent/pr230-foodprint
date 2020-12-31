81fbe6e2d6843849791de7e10abb7a14cec2c334
samsambutdifferent <sam@solutions.to>
2020-12-31 02:19:38 +0000

    carbon calculation

--- src/components/IngredientEditor.vue
+++ src/components/IngredientEditor.vue
@@ -20,13 +20,24 @@
                 </div>
             </div>
 
-           <div class="pt-2 justify-end">
-               <button class="p-2 bg-green-200 rounded"
-               @click="addIngredient">
-                +
-               </button>
-           </div>
+            <div class="pt-2 grid grid-cols-8 gap-1">
+                <div class="col-start-1">
+                    <button
+                    @click="() => calculateCarbon()"
+                    class="w-full p-2 bg-purple-200 rounded disabled:opacity-50"
+                    :disabled=awaitingSemanticMatch >
+                        calculate C02e
+                    </button>
+                </div>
+                <div class="col-end-9">
+                    <button class="p-2 bg-green-200 rounded"
+                    @click="addIngredient">
+                        +
+                    </button>
+                </div>
+            </div>
         </div>
+
     </div>
 </template>
 
@@ -65,14 +76,25 @@ export default {
         });
     });
   },
+  computed: {
+      awaitingSemanticMatch() {
+        return this.ingredients.length === 0 
+        || this.ingredients.some(function(element) {
+            if(element.matched === '' || element.weight == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        })
+      }
+  },
   methods: {
       removeRow(index) {
         let previousRow = this.ingredients
         this.newIngredients = previousRow
         
         this.ingredients.splice(index, 1)
-        
-        console.log(previousRow)
       },
       addIngredient() {
           this.ingredients.push({
@@ -96,6 +118,18 @@ export default {
               weight: this.ingredients[index].weight,
               matched: mx.matched
             })
+      },
+      async calculateCarbon() {
+
+        let param = this.ingredients.map(x => { return({'name': x.matched, 'weight': x.weight}) })
+
+        let val = await axios.post(process.env.VUE_APP_CARBON_CALCULATOR, param)
+          .then(function (response) {
+            return response.data;
+        });
+
+        this.$emit('carbonValue', val)
+        this.$emit('forwardPage')
       },
   }
 }
