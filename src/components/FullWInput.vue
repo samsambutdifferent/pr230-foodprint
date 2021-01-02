19d05b1d229538a9bc4089e1822ebe3e36f77ab0
samsambutdifferent <sam@solutions.to>
2021-01-02 20:06:16 +0000

    full width input floating placeholder

--- src/components/FullWInput.vue
+++ src/components/FullWInput.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="w-full my-2 p-1 bg-white flex">
-        <input class="w-full border-b-2 border-gray-300 bg-white h-10 px-5 pr-16 focus:outline-none"
-        type="text" :name="nameProp" v-model="value" @input="valueChanged" :placeholder="placeholderProp"
-        autocomplete="off">
-    </div>  
+    <div class="relative my-4 border-b-2 focus-within:border-blue-500">
+      <input type="text" :name="nameProp" placeholder=" " v-model="value" @input="valueChanged" autocomplete="off"
+      class="block w-full appearance-none focus:outline-none bg-transparent" />
+      <label :for="nameProp" class="absolute top-0 unselectable text-gray-400">{{placeholderProp}}</label>
+    </div>
 </template>
 
 <script>
@@ -25,4 +25,23 @@ export default {
 </script>
 
 <style scoped>
+input:not(:placeholder-shown) ~ label {
+  @apply transform scale-75 -translate-y-6;
+}
+
+input:focus-within ~ label {
+  @apply text-blue-500;
+}
+
+.unselectable {
+    background: none !important;
+    pointer-events: none;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,17 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      transformOrigin: {
+        "0": "0%",
+      },
+      zIndex: {
+        "-1": "-1",
+      },  
+    },
   },
   variants: {
+    borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
     opacity: ({ after }) => after(['disabled'])
   },
   plugins: [],
