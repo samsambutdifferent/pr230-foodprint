ec58f270c6c85d1837c839fb4c0e99630c29d7c6
samsambutdifferent <sam@solutions.to>
2021-01-03 16:00:29 +0000

    add equivalencies

--- src/components/CarbonOutput.vue
+++ src/components/CarbonOutput.vue
@@ -2,43 +2,95 @@
     <div>
         <div class="w-full pl-20 pb-10">
 
-            <div class="pb-2">
-                co2ekg of meal:
-                <span class="font-bold">{{roundTo3decPlaces(co2e)}}</span>
+            <div class="pb-2 bg-gradient-to-r from-green-400 to-white">
+                <span class="font-bold text-xl">{{roundTo3decPlaces(co2e)}}</span> kg CO<sub>2</sub>e 
+                <div class="col-span-1" v-if="equivalencies .filter(x => x.key === 'smartphone_average_day').length > 0">
+                        <Equivalent
+                        :co2eProp=co2ePerYear
+                        :equivalentProp="equivalencies.filter(x => x.key === 'smartphone_average_day')[0]"
+                        />
+                </div>
             </div>
 
-            <div class="pb-2">
+            <div class="pt-6">
                 Assuming you eat this meal <input class="w-8 bg-gray-200" type="number" v-model=timesEatenPerWeek> times per week
             </div>
 
             </div>
-            <div class="pl-20 grid grid-cols-1 sm:grid-cols-8 gap-2">
-                <div>
-                    1 week: <br>
-                    <span class="font-bold">{{roundTo3decPlaces(co2ePerWeek)}}</span> co2ekg
+            <div class="pl-20 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-2">
+                <div class="grid-cols-1 ">
+                    <div class="col-span-1">
+                        <p>1 week:</p>
+                        <span class="font-bold text-xl">{{roundTo3decPlaces(co2ePerWeek)}}</span> kg CO<sub>2</sub>e 
+                    </div>
+                    <div class="col-span-1" v-if="equivalencies.filter(x => x.key === 'bath_hot_gas_boiler').length > 0">
+                        <Equivalent
+                        :co2eProp=co2ePerWeek
+                        :equivalentProp="equivalencies.filter(x => x.key === 'bath_hot_gas_boiler')[0]"
+                        />
+                    </div>
+                </div>
+                <div class="grid-cols-1 ">
+                    <div class="col-span-1">
+                        <p>1 year:</p>
+                        <span class="font-bold text-xl">{{roundTo3decPlaces(co2ePerYear)}}</span> kg CO<sub>2</sub>e 
+                    </div>
+                    <div class="col-span-1" v-if="equivalencies .filter(x => x.key === '1_litre_petrol').length > 0">
+                        <Equivalent
+                        :co2eProp=co2ePerYear
+                        :equivalentProp="equivalencies.filter(x => x.key === '1_litre_petrol')[0]"
+                        />
+                    </div>
                 </div>
                 <div>
-                    1 year: <br>
-                    <span class="font-bold">{{roundTo3decPlaces(co2ePerYear)}}</span> co2ekg
-                </div>
-                <div>
-                    lifetime ({{avgUKLifespan}} years UK average): <br>
-                     <span class="font-bold">{{roundTo3decPlaces(co2ePerLifetime)}}</span> co2ekg
+                    <div class="grid-cols-1 ">
+                        <div class="col-span-1">
+                            <p>lifetime ({{avgUKLifespan}} years UK average): </p>
+                            <span class="font-bold text-xl">{{roundTo3decPlaces(co2ePerLifetime)}}</span> kg CO<sub>2</sub>e 
+                        </div>
+                        <div class="col-span-1" v-if="equivalencies .filter(x => x.key === 'london_to_glasgow_in_car').length > 0">
+                            <Equivalent
+                            :co2eProp=co2ePerLifetime
+                            :equivalentProp="equivalencies.filter(x => x.key === 'london_to_glasgow_in_car')[0]"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
-    </div>
+        </div>
 </template>
 
 <script>
+import { db } from '../firebaseDB.js';
+import Equivalent from './Equivalent.vue'
+import roundTo3decPlaces from '../mixins/helper'
 
 export default {
   name: 'CarbonOutput',
-  inject: ['carbonValue'],
+  inject: ['carbonValue'],  
+  mixins: [roundTo3decPlaces],
+  components: {
+      Equivalent
+  },
+  mounted() {
+    db.collection('equivalency_conversion_factors').onSnapshot((snapshotChange) => {
+      this.equivalencies= [];
+      snapshotChange.forEach((doc) => { 
+        this.equivalencies.push({
+          key: doc.id,
+          type: doc.data().type,
+          unit: doc.data().unit,
+          value: doc.data().value
+        })
+      });
+    });
+  },
   data() {
     return {
         co2e: this.carbonValue,
         timesEatenPerWeek: 2,
-        avgUKLifespan: 79.4
+        avgUKLifespan: 79.4,
+        equivalencies: []
     }
   },
   computed: {
@@ -51,11 +103,6 @@ export default {
       co2ePerLifetime() {
           return this.co2ePerYear * this.avgUKLifespan
       },
-  },
-  methods: {
-      roundTo3decPlaces(val) {
-          return parseFloat(val).toFixed(3)
-      }
   }
 }
 </script>
---
+++ src/components/Equivalent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-if="equivalent !== {}">
+        is equal to: {{roundTo3decPlaces(equivalentValue)}} {{units}}        
+    </div>
+</template>
+
+<script>
+import roundTo3decPlaces from '../mixins/helper'
+
+export default {
+  name: 'CarbonOutput',
+  mixins: [roundTo3decPlaces],
+  props: ['co2eProp', 'equivalentProp'],
+  data() {
+    return {
+        co2e: this.co2eProp,
+        equivalent: this.equivalentProp
+    }
+  },
+  computed: {
+      equivalentValue() {
+          return this.co2e / this.equivalentProp['value'] 
+      },
+      units() {
+          return this.equivalent['unit']
+      }
+  },
+}
+</script>
+
+<style scoped>
+</style>
