709e57f018d7ae38a617cec9f590f13bf86e2eae
samsambutdifferent <sam@solutions.to>
2021-05-03 17:49:05 +0100

    number with commas mixin

--- src/mixins/helper.js
+++ src/mixins/helper.js
@@ -1,7 +1,10 @@
 export default {
     methods: {
         roundTo3decPlaces(val) {
-            return parseFloat(val.toFixed(3))
-      }
+          return parseFloat(val.toFixed(3))
+        },
+        numberWithCommas(x) {
+          return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
     }
   };
