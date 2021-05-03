604e093e720e290137f0bc7b8d680c4010a6d066
samsambutdifferent <sam@solutions.to>
2021-05-03 17:55:50 +0100

    output rounded to 2 dec

--- src/mixins/helper.js
+++ src/mixins/helper.js
@@ -1,7 +1,13 @@
 export default {
     methods: {
+        rounding(val) {
+          return Math.round(val)
+        },
         roundTo3decPlaces(val) {
           return parseFloat(val.toFixed(3))
+        },
+        roundTo2decPlaces(val) {
+          return parseFloat(val.toFixed(2))
         },
         numberWithCommas(x) {
           return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
